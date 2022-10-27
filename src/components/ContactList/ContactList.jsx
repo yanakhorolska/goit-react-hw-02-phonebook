a43f645a6d7aa385608c0d1300cdd34efd5f2fa9
yanakhorolska <yana.khorolska@protonmail.com>
2022-10-27 19:16:22 +0200

    update all

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,23 @@
+﻿import './ContactList.css';
+
+const ContactList = ({ contacts, deleteContact }) => {
+  return (
+    <ul className="List_box">
+      {contacts.map(contact => (
+        <li className="List_item" key={contact.id}>
+          <span>
+            {contact.name}: <span className="List_span">{contact.number}</span>
+          </span>
+          <button
+            className="List_button"
+            onClick={() => deleteContact(contact.id)}
+          >
+            delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,17 @@
+﻿import './Filter.css';
+
+const Filter = ({ value, filterChanger }) => {
+  return (
+    <div className="Filter_box">
+      <p className="Filter_title">Find contacts by name</p>
+      <input
+        className="Filter_input"
+        type="text"
+        value={value}
+        onChange={filterChanger}
+      />
+    </div>
+  );
+};
+
+export default Filter;
